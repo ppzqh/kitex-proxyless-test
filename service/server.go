03453c5f0738ba4abb7bc02fd5fc695fb9cba65e
qiheng.zhou <qiheng.zhou@bytedance.com>
2022-07-03 23:30:19 +0800

    print in server handler

--- service/server.go
+++ service/server.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloudwego/kitex-proxyless-test/codec/thrift/kitex_gen/proxyless"
 	"github.com/cloudwego/kitex-proxyless-test/codec/thrift/kitex_gen/proxyless/greetservice"
 	"github.com/cloudwego/kitex/server"
@@ -15,6 +16,7 @@ func (s *GreetServiceImpl) SayHello(ctx context.Context, request *proxyless.Hell
 	// TODO: Your code here...
 	resp = proxyless.NewHelloResponse()
 	resp.SetMessage("Hello!")
+	fmt.Println("SayHello is called")
 	return
 }
 
